1aef3956a05526181c8e724d5e9569e63583a026
CallumBlair <s5330191@bournemouth.ac.uk>
2023-11-23 12:33:33 +0000

    Environment Key addition
    Added environment keys to store the API key.

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Pre-Interview Coding Exercise for the Junior DevOps Engineer (Tech01295) graduat
 
 To use the program please setup the following enviroment variable with your API key in command prompt:
       
-      setx x-api-key “<yourkey>”
+      setx x-api-key <yourkey>
+
+Please replace <yourkey> with your API access key.
+
+If this is not possible you will be able to enter your key on running the application.
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import constants as c
 import query as q
 import requests
 import string
 import json
+import os
+
 
 address = "https://durvp011gk.execute-api.eu-west-1.amazonaws.com/v1/api/forecasts?city=<name>"
 
 minMaxTemps = {"city":"undefined", "timeSeries": []}
+
+if os.environ.get("x-api-key") == None:
+    os.environ["x-api-key"] = input("Please enter your API Key now for temporary access: ")
 
 city = ""
 while city == "":
@@ -18,27 +22,31 @@ minMaxTemps["city"] = city
 
 
 addressCity = address.replace("<name>", city)
-response = q.query(addressCity, c.apiKey)
+response = q.query(addressCity, os.environ.get("x-api-key"))
 print("Response recieved")
 
-for x in response["features"][0]["properties"]["timeSeries"]:
-    date = x["time"].split("T")[0]
-    temperature = x["screenTemperature"]
-    if date not in [i["date"] for i in minMaxTemps["timeSeries"]]:
+try:
+    if response["message"] == "Forbidden":
+        print("Access forbidden - please check the entered key")
+except:    
+    for x in response["features"][0]["properties"]["timeSeries"]:
+        date = x["time"].split("T")[0]
+        temperature = x["screenTemperature"]
+        if date not in [i["date"] for i in minMaxTemps["timeSeries"]]:
 
-        minMaxTemps["timeSeries"].append({"date":date,
-                                       "minTemp":temperature,
-                                       "maxTemp":temperature})
-    else:
-        index = [i["date"] for i in minMaxTemps["timeSeries"]].index(date)
-        if minMaxTemps["timeSeries"][index]["minTemp"] > temperature:
-            minMaxTemps["timeSeries"][index]["minTemp"] = temperature
-            
-        if minMaxTemps["timeSeries"][index]["maxTemp"] < temperature:
-            minMaxTemps["timeSeries"][index]["maxTemp"] = temperature
+            minMaxTemps["timeSeries"].append({"date":date,
+                                           "minTemp":temperature,
+                                           "maxTemp":temperature})
+        else:
+            index = [i["date"] for i in minMaxTemps["timeSeries"]].index(date)
+            if minMaxTemps["timeSeries"][index]["minTemp"] > temperature:
+                minMaxTemps["timeSeries"][index]["minTemp"] = temperature
+                
+            if minMaxTemps["timeSeries"][index]["maxTemp"] < temperature:
+                minMaxTemps["timeSeries"][index]["maxTemp"] = temperature
 
 
-with open("minMaxTemperatures.json", "w") as output:
-    json.dump(minMaxTemps, output)
+    with open("minMaxTemperatures.json", "w") as output:
+        json.dump(minMaxTemps, output)
 
-print("Output saved to minMaxTemperatures.json")
+    print("Output saved to minMaxTemperatures.json")
